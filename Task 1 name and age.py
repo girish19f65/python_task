9df71ae1ca060fa336e34b7ca42d373cb58ec280
girish19f65 <129709261+girish19f65@users.noreply.github.com>
2025-11-11 15:10:42 +0530

    Add files via upload

---
+++ Task 1 name and age.py
@@ -0,0 +1,5 @@
+# Create a complete program that asks for a user's name and age.
+name = input("enter your user name :")
+age = int(input("enter your age :"))
+# print greeting
+print(f"Hi {name}, your age is {age},  Thank you")
